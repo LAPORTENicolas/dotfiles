34042ecc642a69496a63bb68d1b5a99e215532fc
LAPORTENicolas <laportenicolas@protonmail.com>
2025-09-02 09:13:27 +0200

    Corr

--- polybar/scripts/get_mem.sh
+++ polybar/scripts/get_mem.sh
@@ -1,12 +1,11 @@
-free_mem=$(cat /proc/meminfo | grep MemFree | awk '{print $2}' | sed 's/ kB//')
-used_mem=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}' | sed 's/ kB//')
+mem_free=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}' | sed 's/ kB//')
 mem_total=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}' | sed 's/ kB//')
-let mem_free_total=$free_mem+$used_mem
+let used_mem=$mem_total-$mem_free
 
-if [ $(expr $mem_total / 2) -lt $mem_free_total ]; then
-	printf "%%{F#FE4A49}%%{F-}"
-elif [ $mem_free_total -lt $(expr $mem_total / 2) ]; then
-	printf "%%{F#F08A4B}%%{F-}"
-elif [ $mem_free_total -lt $(expr $mem_total / 2 - $mem_total / 3) ]; then
+if [ $used_mem -lt $(expr $mem_total / 3) ]; then
 	printf "%%{F#00E5E8}%%{F-}"
+elif [ $used_mem -lt $(expr $mem_total / 2) ]; then
+	printf "%%{F#F08A4B}%%{F-}"
+else
+	printf "%%{F#FE4A49}%%{F-}"
 fi
