ec53f47af7fd9d6ccf66272ec0496171a3256127
LAPORTENicolas <laportenicolas@protonmail.com>
2025-09-03 17:24:09 +0200

    update

--- polybar/scripts/get_battery.sh
+++ polybar/scripts/get_battery.sh
@@ -1,26 +1,45 @@
 perc=$(cat /sys/class/power_supply/BAT1/capacity)
 
 if [ $(cat /sys/class/power_supply/ACAD/online) -eq 1 ]; then
-	if [ $perc -lt 79 ]; then
-		printf "%%{F#54D534}󰂐 %%{F-}"
+	if [ $PRINT_BAT -eq 1 ]; then
+		if [ $perc -lt 79 ]; then
+			printf "%%{F#54D534}󰂐 $perc%%%%{F-}"
+		else
+			printf "%%{F#54D534}󱟢 $perc%%%%{F-}"
+		fi
 	else
-		printf "%%{F#54D534}󱟢%%{F-}"
+		if [ $perc -lt 79 ]; then
+			printf "%%{F#54D534}󰂐 %%{F-}"
+		else
+			printf "%%{F#54D534}󱟢 %%{F-}"
+		fi
 	fi
 	exit 0
 fi
 
 rand=$((RANDOM % 2 + 1))
-
 if [ $perc -lt 30 ]; then
 	if [ $rand -eq 1 ]; then
-		printf "%%{F#FE4A49} %%{F-}"
+		printf "%%{F#FE4A49} $perc%%%%{F-}"
 	else
-		printf "%%{F#FE4A49}$perc%%%%{F-}"
+		printf "%%{F#FE4A49}%%{F-}"
 	fi
 elif [ $perc -lt 40 ]; then
-	printf "%%{F#F08A4B} %%{F-}"
+	if [ $PRINT_BAT -eq 1 ]; then
+		printf "%%{F#F08A4B} $perc%%%%{F-}"
+	else
+		printf "%%{F#F08A4B} %%{F-}"
+	fi
 elif [ $perc -lt 75 ]; then
-	printf "%%{F#00E5E8} %%{F-}"
+	if [ $PRINT_BAT -eq 1 ]; then
+		printf "%%{F#00E5E8} $perc%%%%{F-}"
+	else
+		printf "%%{F#00E5E8} %%{F-}"
+	fi
 else
-	printf "%%{F#007C77} %%{F-}"
+	if [ $PRINT_BAT -eq 1 ]; then
+		printf "%%{F#007C77} $perc%%%%{F-}"
+	else
+		printf "%%{F#007C77} %%{F-}"
+	fi
 fi
