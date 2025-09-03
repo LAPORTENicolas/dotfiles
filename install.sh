639be74d715240d7ccdc71e5e9001fdfe663f62d
LAPORTENicolas <laportenicolas@protonmail.com>
2025-09-03 16:31:05 +0200

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
-sudo pacman -S cronie vim
+sudo pacman -S xorg-server xorg-init xorg mesa nvdia nvdia-dkms nvidia_prime nvdia-utils prime-run cronie vim bspwm sxhkd clang gcc feh firefox fzf git htop nvtop kitty alacritty tmux lm_sensors ripgrep make openvpn openssh pavucontrol perl polybar stress tree-sitter unzip zip
 
-sudo systemctl enable cronie
-sudo systemctl start cronie
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+#sudo systemctl enable cronie
+#sudo systemctl start cronie
+
+# https://github.com/allusive-dev/compfy
